65b20a8cbe0e875557163498af775214016157c3
vhamet <valentin.hamet@gmail.com>
2023-07-28 15:59:56 +0200

    basic apollo setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
-import React from "react";
-import logo from "./logo.svg";
+import { gql, useQuery } from "@apollo/client";
+
 import "./App.css";
 
-function App() {
+const PLAYERS_QUERY = gql`
+  query {
+    players {
+      id
+      firstname
+      lastname
+      shortname
+      sex
+      country {
+        code
+      }
+      stats {
+        rank
+        points
+        weight
+        height
+        age
+      }
+    }
+  }
+`;
+
+interface Player {
+  id: string;
+  shortname: string;
+}
+
+interface PlayersData {
+  players: Player[];
+}
+
+const App = () => {
+  const { error, loading, data } = useQuery<PlayersData>(PLAYERS_QUERY);
+
+  const players = data?.players || [];
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold text-blue-800">Eurosport</h1>
       </header>
-      <main></main>
+      <main>
+        {error ? (
+          <div className="text-red-800">Error</div>
+        ) : loading ? (
+          <div className="text-white">Loading...</div>
+        ) : (
+          <>
+            {players.map(({ id, shortname }) => (
+              <div key={id}>{shortname}</div>
+            ))}
+          </>
+        )}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import App from "./App";
+
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://kf9p4bkih6.execute-api.eu-west-1.amazonaws.com/dev/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
