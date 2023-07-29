814e81566f0629b8435c91cbe032f952149cdd15
vhamet <valentin.hamet@gmail.com>
2023-07-29 13:05:57 +0200

    setup prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    // Define your custom ESLint rules here, if needed.
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
